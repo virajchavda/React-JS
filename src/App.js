4a54754df19f347b63e046f9c2b755382c7c6117
virajchavda <virajchavda1807@gmail.com>
2022-07-10 09:31:59 -0700

    update app.js file

--- src/App.js
+++ src/App.js
@@ -3,23 +3,13 @@ import './App.css';
 
 function App() {
   return (
+    <>
     <h1> Viraj Chavda</h1>
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
+    <div>
+
+    <h2> i am CSE Student</h2>
+    </div>
+    </>
   );
 }
 
